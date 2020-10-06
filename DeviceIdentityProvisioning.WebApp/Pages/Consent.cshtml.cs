5250aca74cdb6b1775fdff95b81950917e56fcd0
Jelle Druyts <jelle_druyts@yahoo.com>
2020-10-06 14:28:04 +0200

    Minor improvements to revoking consent

--- DeviceIdentityProvisioning.WebApp/Pages/Consent.cshtml.cs
+++ DeviceIdentityProvisioning.WebApp/Pages/Consent.cshtml.cs
@@ -11,14 +11,16 @@ namespace DeviceIdentityProvisioning.WebApp
     public class ConsentModel : PageModel
     {
         private readonly ILogger<ConsentModel> logger;
+        public string NotificationMessage { get; set; }
 
         public ConsentModel(ILogger<ConsentModel> logger)
         {
             this.logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(string notificationMessage)
         {
+            this.NotificationMessage = notificationMessage;
         }
 
         public async Task<IActionResult> OnPost()
@@ -26,9 +28,18 @@ namespace DeviceIdentityProvisioning.WebApp
             // Revoke consent for the entire organization by removing the Service Principal of the application in the end user's tenant.
             var graphService = GraphServiceClientFactory.GetForUserIdentity(this.User);
             var deviceIdentityProvisioningAppId = Startup.DeviceIdentityProvisioningAppId;
-            var deviceIdentityProvisioningServicePrincipal = (await graphService.ServicePrincipals.Request().Filter($"appId eq '{deviceIdentityProvisioningAppId}'").GetAsync()).Single();
-            await graphService.ServicePrincipals[deviceIdentityProvisioningServicePrincipal.Id].Request().DeleteAsync();
-            return RedirectToPage();
+            var deviceIdentityProvisioningServicePrincipal = (await graphService.ServicePrincipals.Request().Filter($"appId eq '{deviceIdentityProvisioningAppId}'").GetAsync()).SingleOrDefault();
+            var notificationMessage = default(string);
+            if (deviceIdentityProvisioningServicePrincipal != null)
+            {
+                await graphService.ServicePrincipals[deviceIdentityProvisioningServicePrincipal.Id].Request().DeleteAsync();
+                notificationMessage = "Consent was revoked successfully. Please sign out.";
+            }
+            else
+            {
+                notificationMessage = "Consent was already revoked. Please sign out.";
+            }
+            return RedirectToPage(new { notificationMessage = notificationMessage });
         }
     }
 }
